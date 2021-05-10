1273305a47e148afd606b00bbfa1ddb32b2d61be
Midhili2003 <83817099+Midhili2003@users.noreply.github.com>
2021-05-10 23:53:32 +0530

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -1,5 +1,10 @@
 import time
-n = int(input("Enter the initial countdown time(in seconds): "))
-for i in range(n, -1,-1 ):
-    print(i, end="\r")
+n = int(input("For how many seconds do you want to set the timer?"))
+while n:
+    mins = n//60
+    secs = n%60
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    print(timer, end="\r")
+    n -= 1
     time.sleep(1)
+print("Blast off!!")
